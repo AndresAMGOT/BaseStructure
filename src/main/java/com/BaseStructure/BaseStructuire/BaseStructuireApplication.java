306966ee5b0b0b223686bcdf7a4c9ab16315c6bf
Andres <jalvarado60606@ufide.ac.cr>
2024-06-11 19:15:32 -0700

    [FI-001] I create the spring project

---
+++ src/main/java/com/BaseStructure/BaseStructuire/BaseStructuireApplication.java
@@ -0,0 +1,13 @@
+package com.BaseStructure.BaseStructuire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseStructuireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseStructuireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BaseStructure/BaseStructuire/BaseStructuireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaseStructure.BaseStructuire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseStructuireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
